c4debefaa8fc667b861523e22db88f268349e54a
Samuel Leonel <samueleonelfreitas@hotmail.com>
2020-06-23 23:12:10 -0300

    Resolução do desafio - minha solução

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -14,15 +14,36 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = {
+      "income": 0,
+      "outcome": 0,
+      "total": 0
+    }
+
+    this.transactions.map(transaction => {
+      if(transaction.type === 'income'){
+        balance.income = transaction.value + balance.income;
+        balance.total = transaction.value + balance.total;
+      } else {
+        balance.outcome = transaction.value + balance.outcome;
+        balance.total = balance.total - transaction.value;
+      }
+    })
+
+    return balance;
+
   }
 
-  public create(): Transaction {
-    // TODO
+  public create(title: string, value: number, type: 'income' | 'outcome' ): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
